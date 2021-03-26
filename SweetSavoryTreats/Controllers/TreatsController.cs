4fc8d75f64c54e9856410b3b3d7d38e61c3e6d36
tien96ng <tien96ng@gmail.com>
2021-03-26 09:21:23 -0700

    Initialize TreatsController.cs with boilerplate many to many relationship

---
+++ SweetSavoryTreats/Controllers/TreatsController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Recipe.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace SweetSavoryTreats.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly SweetSavoryTreatsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public UsersController(UserManager<ApplicationUser> userManager, SweetSavoryTreatsContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+  }
+}
