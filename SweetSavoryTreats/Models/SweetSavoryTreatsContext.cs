7cf2aaf9baabd8b6a4c3dd416af709a74e683533
tien96ng <tien96ng@gmail.com>
2021-03-26 09:02:53 -0700

    Initialize SweetSavoryTreatsContext.cs

---
+++ SweetSavoryTreats/Models/SweetSavoryTreatsContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SweetSavoryTreats.Models
+{
+  public class SweetSavoryTreatsContext : IdentityDbContext<ApplicationUser>
+  {
+    public virtual DbSet<Falvor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreats { get; set; }
+
+    public SweetSavoryTreatsContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
