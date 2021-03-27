e2f5531b5509f68b8528f3ac156bdbc9eb4e3050
tien96ng <tien96ng@gmail.com>
2021-03-26 17:39:08 -0700

    Add db context to HomeController and Index.cshtml to show all Flavors and Treats on the splash page.

--- SweetSavoryTreats/Controllers/HomeController.cs
+++ SweetSavoryTreats/Controllers/HomeController.cs
@@ -1,13 +1,27 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SweetSavoryTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace Recipe.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly SweetSavoryTreatsContext _db;
+    public HomeController(SweetSavoryTreatsContext db)
+    {
+      _db = db;
+    }
 
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.Treats = _db.Treats.ToList();
+      ViewBag.Flavors = _db.Flavors.ToList();
       return View();
     }
   }
